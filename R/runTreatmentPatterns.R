7a23c1dce12cc010f797b5881bd0f9fe55717c89
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-10-28 11:28:39 +0100

    updates

--- R/runTreatmentPatterns.R
+++ R/runTreatmentPatterns.R
@@ -1,4 +1,23 @@
-runTreatmentPatterns <- function(connectinoDetails, resultSchema, cdmSchema, tempEmulationSchema, cohortTableName, outputFolder) {
+#' runTreatmentPatterns
+#'
+#' @param connectinoDetails (`ConnectionDetails`) ConnectionDetails object created with `DatabaseConnector::createConnectionDetails`.
+#' @param resultSchema (`character(1)`) Database Schema where the cohort table exists.
+#' @param cdmSchema (`character(1)`) Database schema wherethe CDM exists.
+#' @param tempEmulationSchema (`character(1)`) Temporary emulation schema as described in `TreatmentPatterns::computePathways`.
+#' @param cohortTableName (`character(1)`) Name of the cohort table.
+#' @param outputFolder (`character(1)`) Path where the output should be written to.
+#'
+#' @return `NULL`
+#' @export
+runTreatmentPatterns <- function(
+    connectinoDetails,
+    resultSchema,
+    cdmSchema,
+    tempEmulationSchema,
+    cohortTableName,
+    outputFolder
+  ) {
+  if (!dir.exists(outputFolder)) dir.create(outputFolder, recursive = TRUE, showWarnings = FALSE)
   cohortsBreastCancer <- data.frame(
     cohortId = c(1790368, 1790362, 1790367, 1790366, 1790365, 1790364),
     cohortName = c("Breast Cancer", "Targeted Drug Therapy", "Chemotherapy", "Surgery", "Radiotherapy", "Immunotherapy"),
@@ -16,8 +35,9 @@ runTreatmentPatterns <- function(connectinoDetails, resultSchema, cdmSchema, tem
     `Breast-Cancer-Malignant-Tumor` = cohortsMalignantTumor
   )
 
-  for (i in length(cohortsList)) {
-    label <- names(cohortsList[[i]])
+  for (i in seq_len(length(cohortsList))) {
+    browser()
+    label <- names(cohortsList)[[i]]
 
     outputEnv <- TreatmentPatterns::computePathways(
       cohorts = cohortsList[[i]],
